b92671f0da84538f96597bcf0e94153920ee40a9
András Németh <sunarch@noreply.codeberg.org>
2023-12-19 01:18:46 +0000

    Initial commit

---
+++ src/projectname/__main__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# imports: library
+from argparse import ArgumentParser
+
+# imports: dependencies
+from libmonty_logging.config.file_and_stream.v1 import config as logging_config
+import libmonty_logging.helper as logging_helper
+import libmonty_logging.message as logging_message
+
+# imports: project
+from projectname import version
+
+
+def main() -> None:
+
+    logging_helper.apply_config(version.PROGRAM_NAME,
+                                version.__version__,
+                                logging_config)
+
+    logging_message.program_header(version.PROGRAM_NAME)
+
+    parser = ArgumentParser(prog=version.PROGRAM_NAME)
+
+    parser.add_argument('--version',
+                        help='Display version',
+                        action='store_true',
+                        dest='version')
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(f'{version.PROGRAM_NAME} {version.__version__}')
+        return
+
+
+if __name__ == '__main__':
+    main()
